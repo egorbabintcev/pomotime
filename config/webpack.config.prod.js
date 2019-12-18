2d4f674aa699331146b0abcd72dc6efff82403d9
Egor Babintsev <e.e.babintsev@yandex.ru>
2019-12-18 21:07:54 +0300

    Removed eslint from webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -7,21 +7,7 @@ const prodWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: '/node_modules/',
-        loader: 'eslint-loader',
-        options: {
-          cache: true,
-          fix: true
-        }
-      }
-    ]
-  }
+  ]
 })
 
 module.exports = prodWebpackConfig;
